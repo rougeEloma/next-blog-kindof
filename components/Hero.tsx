9c81d99d70d0a19ba362fa5048d11b34421674c1
ALAIN ELOMA <alainelomapro@gmail.com>
2024-03-08 19:52:20 +0100

    hero - done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,29 @@
+'use client';
 import Image from 'next/image'
+import { CustomButton } from '.'
 
 const Hero = () => {
+    const handleScroll = () => {}
   return (
-    <div>Hero</div>
+    <div className='flex-1 pt-36 padding-x'>
+        <div>
+        <h1 className='hero__title'>Trouvez, réservez ou louez une voiture - rapidement et facilement</h1>
+
+        <p className="hero__subtitle">ameliorer votre experience de location de voiture avec notre logiciel exceptionel.</p>
+
+        <CustomButton 
+            title='Explore Cars'
+            containerStyles="bg-primary-blue text-white rounded-full mt-10"
+            handleClick={handleScroll} />
+        </div>
+
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src="/hero.png" alt="hero" fill className='object-contain' />
+            </div>
+            <div className="hero__image-overlay"></div>
+        </div>
+    </div>
   )
 }
 
---
+++ components/NavBar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
