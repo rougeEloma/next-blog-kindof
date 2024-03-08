49c60a26d29ead42cc6f0d743c6b156bece8a898
ALAIN ELOMA <alainelomapro@gmail.com>
2024-03-08 20:08:11 +0100

    navbar - in progress

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,23 @@ import { CustomButton } from "."
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header className="w-full absolute z-10">
+      <nav className="flex max-w-[1200px] mx-auto justify-between items-center sm:px-12 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+          src="/logo.svg"
+          alt="Car logo hub"
+          width={118}
+          height={18}
+          className="object-contain"/>
+        </Link>
+        
+        <CustomButton
+          title="S'inscrire"
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"/>
+      </nav>
+    </header>
   )
 }
 
