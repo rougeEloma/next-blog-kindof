24c0cbab24def5ce578eab3eb737e6cafc07de5d
ALAIN ELOMA <alainelomapro@gmail.com>
2024-03-11 20:42:10 +0100

    car catalogue - started

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero/>
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4x1 font-extrabold">Catalogue de véhicule</h1>
+          <p>Explorez et découvrez les véhicules de vos besoins.</p>
+        </div>
+      </div>
     </main>
   );
 }
